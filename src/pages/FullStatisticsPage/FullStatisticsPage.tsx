336d027c478d836a168c8c7bb96f4ff1de234684
alexeiVinnichek <alexei.vinnichek@mail.ru>
2024-11-02 16:32:48 +0300

    full correct work of full statistics page 1. created new component for statistics data of amount and income 2. created new interface 3. optimised work of full statistics page

--- src/pages/FullStatisticsPage/FullStatisticsPage.tsx
+++ src/pages/FullStatisticsPage/FullStatisticsPage.tsx
@@ -1,32 +1,66 @@
-import { StatisticsPageHeader } from "../../components/StatisticsPageHeader/StatisticsPageHeader";
-import { StatisticsMainWrapper } from "../../components/StatisticsMainWrapper/StatisticsMainWrapper";
-import { StatisticsTopText } from "../../components/StatisticsTopText/StatisticsTopText";
 import { useCallback, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+import { Spinner } from "../../components/Spinner/Spinner";
+import { StatisticsMainWrapper } from "../../components/StatisticsMainWrapper/StatisticsMainWrapper";
+import { StatisticsPageHeader } from "../../components/StatisticsPageHeader/StatisticsPageHeader";
+import { StatisticsTopText } from "../../components/StatisticsTopText/StatisticsTopText";
+import { StatisticsDataContainer } from "../../components/StatisticsDataContainer/StatisticsDataContainer";
 import { SelectContainer } from "../../components/SelectContainer/SelectContainer";
-import { FullStatisticsDataContainer } from "./components/FullStatisticsDataContainer/FullStatisticsDataContainer";
 import { showSnackMessage } from "../../redux/slices/snackMessageSlice/snackMessageSlice";
 import { createSnackMessage } from "../../utils/createSnackMessage";
-import { Spinner } from "../../components/Spinner/Spinner";
 import { startLoading, stopLoading } from "../../redux/slices/loadingSlice/loadingSlice";
-import { IFullStatisticsData } from "../../share/interfaces/fullStatisticsData";
+import { IStatisticsData } from "../../share/interfaces/statisticsData";
 
 const FullStatisticsPage = () => {
+    const [isInitialDataLoaded, setIsInitialDataLoaded] = useState(true);
+    const [commonStatistics, setCommonStatistics] = useState<IStatisticsData | null>(null);
+    const [studentStatistics, setStudentStatistics] = useState<IStatisticsData | null>(null);
     const [studentsNames, setStudentsNames] = useState<string[]>(['']);
-    const [isNamesLoading, setIsNamesLoading] = useState(true);
-    const [data, setData] = useState<IFullStatisticsData | null>(null);
+    const [selectedStudentName, setSelectedStudentName] = useState('');
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        const loadInitialData = async () => {
+            try {
+                const responseOfNames = await fetch(`http://localhost:3002/getStudentsNames`);
+                if (!responseOfNames) {
+                    throw new Error('Server error while loading names');
+                }
+                const studentsNames = await responseOfNames.json();
+
+                const responseOfCommonStatistics = await fetch(`http://localhost:3002/getCommonStatistics`);
+                if (!responseOfCommonStatistics) {
+                    throw new Error('Server error while loading common statistics');
+                }
+                const commonStatistics = await responseOfCommonStatistics.json();
+
+                setStudentsNames(studentsNames);
+                setCommonStatistics(commonStatistics);
+            } catch (error) {
+                dispatch(showSnackMessage(createSnackMessage(
+                    `Error while loading initial data: ${error instanceof Error ?
+                        error.message :
+                        'unknown error occured'
+                    }!`,
+                    'error'
+                )));
+            } finally {
+                setIsInitialDataLoaded(false);
+            }
+        }
+        loadInitialData();
+    }, [dispatch]);
 
     const fetchStudentData = useCallback(async (name: string) => {
         dispatch(startLoading());
         try {
-            const response = await fetch(`http://localhost:3002/getfullStatistics?name=${name}`);
+            const response = await fetch(`http://localhost:3002/getFullStudentStatistics?name=${name}`);
             if (!response.ok) {
                 throw new Error(`Server error while getting data for ${name}!`);
 
             }
             const data = await response.json();
-            setData(data);
+            setStudentStatistics(data);
         } catch (error) {
             dispatch(showSnackMessage(createSnackMessage(
                 `Error while loading statistics data: ${error instanceof Error ?
@@ -40,36 +74,8 @@ const FullStatisticsPage = () => {
         }
     }, [dispatch]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`http://localhost:3002/getStudentsNames`);
-                if (!response.ok) {
-                    throw new Error('Server Error while loading names for students!');
-                }
-
-                const data = await response.json();
-                setStudentsNames(data);
-                setIsNamesLoading(false);
-
-                if (data.length > 0) {
-                    await fetchStudentData(data[0]);
-                }
-            } catch (error) {
-                dispatch(showSnackMessage(createSnackMessage(
-                    `Error while loading: ${error instanceof Error ?
-                        error.message :
-                        'unknown error occurred!'
-                    }!`,
-                    'error'
-                )));
-                setIsNamesLoading(false);
-            }
-        };
-        fetchData();
-    }, [dispatch, fetchStudentData]);
-
     const onselectStudentChange = useCallback(async (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedStudentName(e.target.value);
         await fetchStudentData(e.target.value);
     }, [fetchStudentData]);;
 
@@ -79,15 +85,30 @@ const FullStatisticsPage = () => {
                 description="Here you can find statistics for full time: amount of lessons and full income. In the bottom part choose student and find income and amount of lessons for interested student!"
             />
             {
-                isNamesLoading ?
+                isInitialDataLoaded ?
                     <Spinner /> :
                     <StatisticsMainWrapper>
                         <StatisticsTopText value="Full Statistics" />
                         <SelectContainer
-                            data={studentsNames}
                             onChange={onselectStudentChange}
+                            data={studentsNames}
+                            value={selectedStudentName}
+                            initialOption="ChooseStudent"
                         />
-                        {data && <FullStatisticsDataContainer data={data} />}
+                        <StatisticsDataContainer
+                            amountLabel="Common amount"
+                            amount={commonStatistics?.amountOfLessons}
+                            incomeLabel="Common income"
+                            income={commonStatistics?.income}
+                        />
+                        {studentStatistics && (
+                            <StatisticsDataContainer
+                                amountLabel="Student amount"
+                                amount={studentStatistics.amountOfLessons}
+                                incomeLabel="Student income"
+                                income={studentStatistics.income}
+                            />
+                        )}
                     </StatisticsMainWrapper>
             }
         </>
---
+++ src/share/interfaces/statisticsData.ts
@@ -0,0 +1,4 @@
+export interface IStatisticsData {
+    amountOfLessons: number;
+    income: number;
+};
