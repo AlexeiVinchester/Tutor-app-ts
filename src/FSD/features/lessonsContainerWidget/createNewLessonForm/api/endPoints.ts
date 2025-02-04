3be6fcaf2aa0135dd8b91e9601d34ca4c3e7bb42
alexeiVinnichek <alexei.vinnichek@mail.ru>
2025-02-04 14:07:43 +0300

    Tutor-app/lessons-page: updated endpoints

--- src/FSD/features/lessonsContainerWidget/createNewLessonForm/api/endPoints.ts
+++ src/FSD/features/lessonsContainerWidget/createNewLessonForm/api/endPoints.ts
@@ -1,4 +1,4 @@
 export const createNewLessonFormEndPoints: Readonly<Record<string, string>> = {
-  loadInitialData: 'http://localhost:3002/getNamesAndNextId',
-  sendNewLesson: 'http://localhost:3002/addLesson',
-};
+  loadInitialData: '/getNamesAndNextId',
+  sendNewLesson: '/addLesson',
+}
