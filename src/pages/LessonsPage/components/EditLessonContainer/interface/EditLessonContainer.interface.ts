0ebc987a3c1b8c3bc224108ba78fba812bee2714
alexeiVinnichek <alexei.vinnichek@mail.ru>
2025-02-05 22:31:21 +0300

    Tutor-app/master: deleted unused files

--- src/pages/LessonsPage/components/EditLessonContainer/interface/EditLessonContainer.interface.ts
+++
@@ -1,7 +0,0 @@
-import { ILesson } from '../../../../../share/interfaces/lesson.interface';
-
-export interface IEditLessonContainerProps {
-  oldLesson: ILesson;
-  isOpen: boolean;
-  close: () => void;
-}
--- src/pages/LessonsPage/components/ShowSnackBarProvider/interface/ShowSnackBarProvider.interface.ts
+++
@@ -1,6 +0,0 @@
-import { IChildren } from '../../../../../share/interfaces/children.interface';
-import { TShowSnackBar } from '../../../../../share/interfaces/showSnackBar.type';
-
-export interface IShowSnackBarProvider extends IChildren {
-  showSnackBar: TShowSnackBar;
-}
