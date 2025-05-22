853079d965512cec9a75c7960ec6e2b2ab818b56
alexeiVinnichek <alexei.vinnichek@mail.ru>
2025-05-23 00:10:44 +0300

    feature(StudentForm): created zod schema for form with validation

---
+++ src/FSD/entities/student/model/studentFormSchema.ts
@@ -0,0 +1,43 @@
+import * as z from 'zod';
+
+const phoneRegex = /^\+375\((25|29|33|44)\)\d{3}-\d{2}-\d{2}$/;
+
+export const studentFormSchema = z.object({
+  name: z.string().nonempty('You should enter name of student'),
+  gender: z.enum(['male', 'female'], {
+    errorMap: () => ({ message: 'Choose gender!' })
+  }),
+  price: z.preprocess(
+    (val) => {
+      if (typeof val === 'string' && val.trim()) {
+        const num = Number(val);
+        return isNaN(num) ? val : num;
+      }
+      return val;
+    },
+    z.number({ invalid_type_error: 'Invalid type, enter number!' })
+      .positive('Price should be a positive number')),
+  form: z.preprocess(
+    (val) => {
+      if (typeof val === 'string' && val.trim()) {
+        const num = Number(val);
+        return isNaN(num) ? val : num;
+      }
+      return val;
+    },
+    z.number({ invalid_type_error: 'Should be a number!' })
+      .int('Should be integer')
+      .min(1, 'Min is 1 form')
+      .max(11, 'Max is 11 form')
+  ),
+  parentName: z.string().nonempty('You should enter name of student'),
+  parentMobilePhone: z
+    .string()
+    .regex(phoneRegex, 'Enter phone in such format: +375(44)111-11-11'),
+  studentMobilePhone: z
+    .string()
+    .regex(phoneRegex, 'Enter phone in such format: +375(44)111-11-11'),
+  activity: z.boolean()
+});
+
+export type TStudentFormSchema = z.infer<typeof studentFormSchema>;
