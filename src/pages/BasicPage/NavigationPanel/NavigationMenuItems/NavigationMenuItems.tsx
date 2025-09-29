d2b8d722b2e885c9f601afcd302b0783aba48760
alexeiVinnichek <alexei.vinnichek@mail.ru>
2025-09-29 23:02:54 +0300

    refactor(NavigationMenuItems): refactoring

--- src/pages/BasicPage/NavigationPanel/NavigationMenuItems/NavigationMenuItems.tsx
+++ src/pages/BasicPage/NavigationPanel/NavigationMenuItems/NavigationMenuItems.tsx
@@ -6,48 +6,29 @@ import TaskIcon from '@mui/icons-material/Task';
 import InfoIcon from '@mui/icons-material/Info';
 import SchoolIcon from '@mui/icons-material/School';
 import { MAIN, STUDENTS, LESSONS, STATISTICS, TASKS, ABOUT } from '../../../../app/Router/routes';
+import { createElement } from 'react';
 
+const navLinkMap = [
+  { icon: HomeIcon, to: MAIN, title: "Main" },
+  { icon: PersonIcon, to: STUDENTS, title: "Students" },
+  { icon: SchoolIcon, to: LESSONS, title: "Lessons" },
+  { icon: EqualizerIcon, to: STATISTICS, title: "Statistics" },
+  { icon: TaskIcon, to: TASKS, title: "Tasks" },
+  { icon: InfoIcon, to: ABOUT, title: "About" },
+];
 
 const NavigationMenuItems = () => {
   return (
     <div className="flex menu-box">
       <ul className="flex justify-between items-center">
-        <li>
-          <NavLink className="menu-item" to={MAIN}>
-            <HomeIcon />
-            Main
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="menu-item" to={STUDENTS}>
-            <PersonIcon />
-            Students
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="menu-item" to={LESSONS}>
-            <SchoolIcon />
-            Lessons
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="menu-item" to={STATISTICS}>
-            <EqualizerIcon />
-            Statistics
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="menu-item" to={TASKS}>
-            <TaskIcon />
-            Tasks
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="menu-item" to={ABOUT}>
-            <InfoIcon />
-            About
-          </NavLink>
-        </li>
+        {
+          navLinkMap.map(navlink => (
+            <NavLink key={navlink.title} className="menu-item" to={navlink.to}>
+              {createElement(navlink.icon)}
+              {navlink.title}
+            </NavLink>
+          ))
+        }
       </ul>
     </div>
   );
